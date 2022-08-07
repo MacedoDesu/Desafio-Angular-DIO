c843b0379f0025d127ca183367c0f85c7f33dd95
MacedoDesu <macedo41701@gmail.com>
2022-08-07 03:09:04 -0300

    Projeto de Angular terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProducListComponent } from './components/produc-list/produc-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    ProducListComponent
   ],
   imports: [
     BrowserModule,
